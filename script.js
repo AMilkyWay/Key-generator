deeb43d8bba9a86fc2461775fd05a77c826c3e27
ROMAHKAO <89422534+AMilkyWay@users.noreply.github.com>
2022-08-02 16:20:34 +0000

    Error fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let type = "num"
 function gen() {
     let key = ""
-    if (type == num) {
+    if (type == "num") {
       for (let i = 0; i < 20; i++) {
         key = key + Math.floor(Math.random() * 10)
       }
